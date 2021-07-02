0a0e347708bb74c79f3fce1f5c8ec4b68086b86e
luisanthony196 <luis.anthony.chavarria@gmail.com>
2021-07-02 13:13:02 -0500

    Modelo simpleKMeans minimamente funcional

---
+++ README.md
@@ -0,0 +1,20 @@
+# API Weka
+API REST desarrollada con el fin de aprovechar las capacidades en minería de datos y aprendizaje automático que brinda la plataforma Weka, y ampliar su uso en navegadores web y dispositivos móviles.
+
+## Requerimientos
+- Java 11
+- Maven
+
+## Conexión a Base de Datos
+- Configurar adecuadamente el archivo **DatabaseUtils.prop**
+- Ingresar el *username* y el *password* en el archivo **application.properties**
+
+## Scripts
+Limpiar e instalar las librerías (dependencias)
+- mvnw.cmd clean install
+
+Ejecutar la aplicación como un servidor
+- mvnw.cmd spring-boot:run
+
+## Rutas
+- localhost:8080/api/percent
--- src/main/java/com/bytecode/core/controller/DataController.java
+++ src/main/java/com/bytecode/core/controller/DataController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import weka.clusterers.SimpleKMeans;
+
 @RestController
 @RequestMapping("/api")
 public class DataController {
@@ -14,6 +16,15 @@ public class DataController {
     
     @GetMapping("/percent")
     public String result() {
+        try {
+            SimpleKMeans model = new SimpleKMeans();
+            model.setNumClusters(5);
+            model.buildClusterer(dataService.obtenerDatos());
+            System.out.println(model);
+            return model.toString();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
         String datos = dataService.obtenerDatos().toString();
         return datos;
     }
